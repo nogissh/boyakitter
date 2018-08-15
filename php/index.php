8c902e108b128227772931f43ecfc8ca43c95dfc
Toshiki Ohnogi <thetoshiki0419@gmail.com>
2018-08-15 15:22:32 +0900

    initial: 最小構成

---
+++ php/index.php
@@ -0,0 +1,12 @@
+<?php
+$head_title = "fuelphp-app";
+$body_content = "hello, world";
+?>
+<html>
+<head>
+<title><?php echo $head_title; ?></title>
+</head>
+<body>
+<p><?php echo $body_content; ?></p>
+</body>
+</html>
